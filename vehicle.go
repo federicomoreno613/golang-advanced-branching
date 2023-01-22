36a6f63ebc411f589cf99e84495a7dc742f4d5d6
federico.moreno1 <federico.moreno@kavak.com>
2023-01-22 20:01:29 -0300

    Completed module

--- vehicle.go
+++ vehicle.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -86,6 +87,20 @@ func main() {
 	// Generate ratings for the different vehicles
 	generateRating()
 	// Print ratings for the different vehicles
+	//for statement with no key, a value named veh, and range of inventory.
+	for _, veh := range inventory {
+		// Create a switch statement for the vehicle type
+		switch v := veh.(type) {
+		case car:
+			v.carDetails()
+		case bike:
+			v.bikeDetails()
+		case truck:
+			v.truckDetails()
+		default:
+			fmt.Printf("Are you sure this Vehicle Type exists")
+		}
+	}
 }
 
 func readJSONFile() Values {
@@ -147,4 +162,37 @@ func generateRating() {
 		// Add rating to the vehicle model
 		vehicleResult[v.Name] = vehResult
 	}
+}
+
+//The first task of this module is to create a function showRating() with one parameter model of type string, and no return value.
+
+func showRating(model string) {
+	ratingFound = false
+	for m, r := range vehicleResult {
+		if m == model {
+			fmt.Printf("Total Ratings:%v\tPositive:%v\tNegative:%v\tNeutral:%v", r.feedbackTotal, r.feedbackPositive, r.feedbackNegative, r.feedbackNeutral)
+			//The second statement will assign the bool value true to the variable ratingFound.
+			ratingFound = true
+		}
+		if !ratingFound {
+			fmt.Println("No rating for this vehicle")
+		}
+
+	}
+}
+
+// create carDetails() method which has a receiver of type *car named c with no parameters and has no return value. We will write two statements within this method
+func (c *car) carDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Car", c.make, c.model)
+	showRating(c.model)
+}
+
+func (b *bike) bikeDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Bike", b.make, b.model)
+	showRating(b.model)
+}
+
+func (t *truck) truckDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Truck", t.make, t.model)
+	showRating(t.model)
 }
